cb519d2c46544a5a3f87bd42fa33670aa731169a
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2021-12-21 10:33:33 -0600

    update project

--- Hieken/PCoA/Starting_PCoA_Analysis.R
+++
@@ -1,7 +0,0 @@
-# Starting PCoA analysis
-# updated Oct 26 2020
-BiocManager::install("hopach")
-#distmat<-distancematrix(seqtab.nochim, d = "euclid", na.rm=TRUE)
-distmat<-dist(seqtab.nochim, method = "euclidean", diag = FALSE, upper = FALSE, p = 2) #distance matrix with ASV table as input
-pc_plot<-pcoa(distmat)
-biplot(pc_plot)

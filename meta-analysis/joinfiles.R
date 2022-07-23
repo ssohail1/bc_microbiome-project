fe653d4b725b1cc3bec10d073f8915bf136e8099
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-07-23 13:49:00 -0500

    Update joinfiles.R

--- meta-analysis/joinfiles.R
+++ meta-analysis/joinfiles.R
@@ -1,30 +1,12 @@
-# # Urbaniak Taxa table
-# Urbaniaktaxa <- read.table("~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/Urbtaxafile.txt",header = TRUE)
-# Urbaniaktaxa <- data.frame(Urbaniaktaxa)
-# Urbaniaktaxa1 <- Urbaniaktaxa[,-1]
-# rownames(Urbaniaktaxa1) <- Urbaniaktaxa[,1]
 # Urbaniak ASV table
 seqtab.nochimudat <- read.table("~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/UrbASVmodifiedSRRs.txt", header = TRUE)
 seqtab.nochimudat <- data.frame(seqtab.nochimudat)
-# seqtab.nochimudat1 <- seqtab.nochimudat[,-1]
-# rownames(seqtab.nochimudat1) <- seqtab.nochimudat[,1]
-# seqtab.nochimudat1 <- t(seqtab.nochimudat1)
+
 # Urbaniak taxa table
 taxasilvaudat <- read.table("~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/Urbtaxafile_R.txt",header = TRUE)
 taxasilvaudat <- data.frame(taxasilvaudat)
-# taxasilvaudat1 <- taxasilvaudat[,-1]
-# rownames(taxasilvaudat1) <- taxasilvaudat[,1]
-# taxasilvaudat1 <- as.matrix(taxasilvaudat1)
+
 # Urbaniak metadata table
-# Urbmetatoremove <- read.table('~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/Urbmetremove42samp.txt',header=TRUE)
-# samdfudat <- read.table("~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/UrbaniakMet--.txt",header = TRUE)
-# samdfudat <- data.frame(samdfudat)
-# healthy <- read.table("~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/listofhea.txt",header = TRUE)
-# healthy <- data.frame(healthy)
-# ben <- read.table("~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/listofben.txt",header = TRUE)
-# ben <- data.frame(ben)
-# ca <- read.table("~/Documents/Urbaniak_02152022_version/MicrobAnalystInputs/listofca.txt",header = TRUE)
-# ca <- data.frame(ca)
 samdfudat <- read.table(file= "~/Downloads/urbaniakmeta.txt")
 samdfudat <- data.frame(samdfudat)
 for (j in 1:length(rownames(samdfudat))) {
@@ -41,18 +23,14 @@ for (j in 1:length(rownames(samdfudat))) {
 # Hieken ASV table
 HiekenASVtab <- read.table('~/Documents/Hieken10082021/MicrobiomeAnalyst_Inputs/seqtabnochimhieken10082021.txt',header = TRUE)
 HiekenASVtab <- data.frame(HiekenASVtab)
-# HiekenASVtab2 <- HiekenASVtab[,-1]
-# rownames(HiekenASVtab2) <- HiekenASVtab[,1]
+
 # Hieken Taxa table
 Hiekentaxa <- read.table('~/Documents/Hieken10082021/MicrobiomeAnalyst_Inputs/taxasilvahieken10082021.txt',header=TRUE)
 Hiekentaxa <- data.frame(Hiekentaxa)
-# Hiekentaxa1 <- Hiekentaxa[,-1]
-# rownames(Hiekentaxa1) <- Hiekentaxa$TAXONOMY
+
 # Hieken metadata table
 HMeta <- read.table("~/Documents/Hieken10082021/MicrobiomeAnalyst_Inputs/HMetadata_Tissues.txt")
 HMeta <- data.frame(HMeta)
-# HMetaHiek <- HMeta[,-1]
-# rownames(HMetaHiek) <- HMeta[,1]
 colnames(HMeta) <- c("sample_id","sample","sample_type")
 colnames(samdfudat) <- c("sample_id","sample","tissue","sample_name","sample_type")
 
@@ -80,10 +58,6 @@ psudat <- phyloseq(otu_table(HiekUrbASVcombined1, taxa_are_rows=FALSE),
                    tax_table(HiekUrbTAXAcombined1))
 
 
-
-
-
-
 match <- list()
 count1 <- 0
 for (i in 1:length(rownames(Urbaniaktaxa1))) {
@@ -95,6 +69,7 @@ for (i in 1:length(rownames(Urbaniaktaxa1))) {
     }
   }
 }
+
 
 
 hiekenGreengenes <- read.table("/media/mbb/Sidras_Projects/Hieken_paper/HiekenGreengenes-joininginR.txt", header = TRUE)
